34abbe482ed7723564ab890f17812b0bf92be5db
Mahdi Mattout <mahdim@lemonadefashion.com>
2022-05-07 20:51:45 +0300

    added the skeleton for the user profile page

--- Controllers/UserAccountController.cs
+++ Controllers/UserAccountController.cs
@@ -1,13 +1,13 @@
-// using System.Diagnostics;
-// using Microsoft.AspNetCore.Mvc;
-// using JMR.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using JMR.Models;
 
-// namespace JMR.Controllers;
+namespace JMR.Controllers;
 
-// public class UserAccountController : Controller
-// {
-//   public IActionResult Index()
-//   {
-//     return View();
-//   }
-// }
+public class UserAccountController : Controller
+{
+  public IActionResult UserProfile()
+  {
+    return View();
+  }
+}
